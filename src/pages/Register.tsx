4e4c5a5aaa9217decf6e3265f22a3cf92c8f20dc
MaherSDev <mahersnobar1@gmail.com>
2026-02-11 18:30:38 +0200

    Form Input Errors

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -10,8 +10,14 @@ interface IFormInput {
 
 // Renders
 const RegisterPage = () => {
-  const { register, handleSubmit } = useForm<IFormInput>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
   const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+	console.log(errors)
 
   return (
     <div className="max-w-md mx-auto">
@@ -20,9 +26,9 @@ const RegisterPage = () => {
       </h2>
 
       <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
-        <Input placeholder="Username" {...register("username")} />
-        <Input placeholder="Email address" {...register("email")} />
-        <Input placeholder="Password" {...register("password")} />
+        <Input placeholder="Username" {...register("username", {required: "Username is required!"})} />
+        <Input placeholder="Email address" {...register("email", {required: "Email is required!"})} />
+        <Input placeholder="Password" {...register("password", {required: "Password is required!"})} />
 
         <Button fullWidth>Register</Button>
       </form>
